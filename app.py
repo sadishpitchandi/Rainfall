bd229b5cc30d17d420257c5c4b71ee5489bb8165
sadishpitchandi <apjsadish2706@gmail.com>
2022-08-22 23:20:56 +0530

    successa

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template, request
 import pandas as pd
 
 app=Flask(__name__)
@@ -9,39 +9,65 @@ def index():
     try:
         return render_template('index.HTML')
     except Exception as e:
-        return str(e)
+        return str(e)  
+
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
 
-    context = {
-        data: None
-    }
+   
       
-    data=pd.read_csv("C:\sadish\project\machine learning\Rainfall\work\CHENNAI\CH_NUNGAMBAKKAM.csv")
-    return data
-    data=data
+    data=pd.read_csv(r"data\\CHENNAI\\CH_NUNGAMBAKKAM.csv")
     
-
+    
+    # html = data.to_html()
+    #print(html)
+    
     if request.method == 'POST':
-    age = int(request.form['age'])
-
-
-
- 
+           data = request.form['data']
     context = {
-             data:data
+             'data':data
         }
+    
+    return render_template("predict.html", context=context)
 
     
-    return render_template('header.html', context=context)
+    # return render_template('header.html', context=context)
+"""
+@app.route('/meenabakkamfile', methods=['GET', 'POST'])
+def predict():
+
+    data=pd.read_csv(r"data\\CHENNAI\\CH_MEENAMBAKKAM.csv")
+    
+    
+    html = data.to_html()
+    
+    
+    if request.method == 'POST':
+           data = request.form['data']
+    context = {
+             'data':data
+        }
+    
+    return render_template("predict.html", context=context)
+
+    """
+
+"""
+@app.route('/extractoftime', methods=['GET', 'POST'])
+def extractoftime():
+    try:
 
 
+      
+        
 
+        return render_template('index.HTML')
+    except Exception as e:
+        return str(e)  
 
-
-
+"""
 
 
 
